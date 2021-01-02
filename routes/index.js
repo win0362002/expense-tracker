b3dfae8e54fc6d691422362d9705e2d656231dea
Mike <win0362002@hotmail.com>
2021-01-03 02:07:19 +0800

    [Routes] Add filter expense record by category feature

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const home = require('./modules/home')
 const records = require('./modules/records')
+const filter = require('./modules/filter')
 
 const router = express.Router()
 
 router.use('/', home)
 router.use('/records', records)
+router.use('/filter', filter)
 
 module.exports = router
---
+++ routes/modules/filter.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const router = express.Router()
+const Record = require('../../models/record')
+const Category = require('../../models/category')
+
+router.post('/', (req, res) => {
+  const targetCategory = req.body.category.trim()
+  let totalAmount = 0
+  let filteredRecords = []
+  let localCategories = []
+  Record.find()
+    .lean()
+    .then((records) => {
+      records.forEach((record) => {
+        if (record.category.trim() === targetCategory) {
+          totalAmount += record.amount
+
+          //Handle date format
+          let newDate = new Date(record.date)
+          const year = newDate.getFullYear()
+          const month = newDate.getMonth()
+          const date = newDate.getDate()
+
+          record.dateString =
+            year +
+            '/' +
+            (month > 8 ? month + 1 : '0' + (month + 1)) +
+            '/' +
+            (date > 9 ? date : '0' + date)
+
+          filteredRecords.push(record)
+        }
+      })
+    })
+    .then(() => {
+      return Category.find()
+        .lean()
+        .then((categories) => {
+          categories.forEach((category) => {
+            if (category.name.trim() !== targetCategory) {
+              localCategories.push(category)
+            }
+          })
+        })
+        .then(() =>
+          res.render('index', {
+            records: filteredRecords,
+            categories: localCategories,
+            totalAmount,
+            targetCategory,
+          })
+        )
+        .catch((error) => console.log(error))
+    })
+    .catch((error) => console.log(error))
+})
+
+module.exports = router
